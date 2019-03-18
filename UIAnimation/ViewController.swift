8a9f0ec39756378a9df1631b5614e167dca9b63d
Emmett <Ehasley1136@stu.d214.org>
2019-03-18 11:21:31 -0500

    Initial Commit

---
+++ UIAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIAnimation
+//
+//  Created by Emmett Hasley on 3/18/19.
+//  Copyright © 2019 John Heresy High School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
